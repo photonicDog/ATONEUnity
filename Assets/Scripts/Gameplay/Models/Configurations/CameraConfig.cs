d6c290d17bbd1c1674b3bbaba5506d19bf5d1ebb
freya <freya@exasteria.net>
2023-07-24 22:51:58 +0100

    feat: Add CameraConfig

---
+++ Assets/Scripts/Gameplay/Models/Configurations/CameraConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Gameplay.Models.Configurations
+{
+    public class CameraConfig
+    {
+        public CameraConfig() { }
+
+        public float Speed = 5f;
+        public float FocalDistance = 10f;
+        public float PitchMax = 70f;
+        public float PitchMin = 45f;
+    }
+}
